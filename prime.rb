70b2c791475d8b070883f1af641e92eb1f24c74c
Charlotte Neff <charlotte.neff@gmail.com>
2019-02-22 23:35:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,4 +2,5 @@ def prime? (num)
   if num < 0 || num = 0 || num = 1
     returns false 
     else 
-      (2..1).to_a.all? do |# Add  code here!
+      (2..1).to_a.all? do |possible_factor|
+        # Add  code here!
